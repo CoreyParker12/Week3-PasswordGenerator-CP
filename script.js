6ca506dd9e0afd395bf358c8ba09b627da250d18
Corey Parker <coreyparker012@gmail.com>
2021-05-04 20:37:28 -0400

    Uploading before I change rand char calcs

--- script.js
+++ script.js
@@ -15,39 +15,54 @@ const uppercaseElement = document.getElementById("uppercaseSelect");
 const lowercaseElement = document.getElementById("lowercaseSelect");
 const numbersElement = document.getElementById("numbersSelect");
 const symbolsElement = document.getElementById("symbolsSelect");
-
-
-const uppercaseOutput = uppercase.charAt(Math.floor(Math.random() * uppercase.length))
-const lowercaseOutput = lowercase.charAt(Math.floor(Math.random() * lowercase.length))
-const numbersOutput = numbers.charAt(Math.floor(Math.random() * numbers.length))
-const symbolsOutput = symbols.charAt(Math.floor(Math.random() * symbols.length))
-
+const passwordElement = document.getElementById("#generate");
 
 // Write password to the #password input
 
 function writePassword() {
   
   /* Reference 1. */ 
-  const length = document.getElementById("lengthSelect").value
+  const pickLength = +document.getElementById("lengthSelect").value
 
-  console.log(length)
+//Going to remove the below method of finding random values
 
   const uppercaseValue = uppercase[Math.floor(Math.random() * uppercase.length)]
   const lowercaseValue = lowercase[Math.floor(Math.random() * lowercase.length)]
   const numbersValue = numbers[Math.floor(Math.random() * numbers.length)]
   const symbolsValue = symbols[Math.floor(Math.random() * symbols.length)]
 
-  console.log(uppercaseValue)
-  console.log(lowercaseValue)
-  console.log(numbersValue)
-  console.log(symbolsValue)  
+
+    const len = pickLength;
+    let pwd = "";
+
+    for (let i = 0; i < len; i++)  {
+      const tempA = generateTemp();
+      pwd += tempA;
+
+    }
+
+  function generateTemp() {
+    const tempB = [];
+    uppercaseElement.checked && tempB.push(uppercaseValue);
+    lowercaseElement.checked && tempB.push(lowercaseValue);
+    numbersElement.checked && tempB.push(numbersValue);
+    symbolsElement.checked && tempB.push(symbolsValue);
+
+    if (tempB.length === 0) {
+      return "";
+    }
+
+    const testing = tempB[Math.floor(Math.random() * tempB.length)];
+    console.log(testing)
+  }
+  
+    //var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = pwd;
 
 }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 
 
